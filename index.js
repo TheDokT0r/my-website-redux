420315589d30c4b87ccffb5d69829dea3dbc64c9
TheDokTor <orkv1234@gmail.com>
2023-05-02 01:38:47 +0300

    Added dark mode button cause why not lol

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const title = document.getElementById("title");
 const subtitle = document.getElementById("subtitle");
+const darkModeBtn = document.getElementById("dark_mode_btn");
+let darkMode = localStorage.getItem("darkMode");
 
 const changeTitle = () => {
     title.textContent = "Don't you have anything better to do?";
@@ -9,3 +11,15 @@ const changeTitle = () => {
 title.addEventListener("click", changeTitle);
 
 subtitle.addEventListener("click", changeTitle);
+
+// Dark Mode
+darkModeBtn.addEventListener("load", function () {
+    if (localStorage.getItem("darkMode") === "true") {
+        darkModeBtn.textContent = "Switch Dark Mode";
+        darkMode = true;
+        return;
+    }
+
+    darkModeBtn.textContent = "Switch Light Mode";
+    darkMode = false;
+});
