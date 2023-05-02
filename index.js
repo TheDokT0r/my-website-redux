33d96d857a149396d0e8d27771ea75f44407c053
TheDokTor <orkv1234@gmail.com>
2023-05-02 11:37:12 +0300

    minor style improvments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const title = document.getElementById("title");
 const subtitle = document.getElementById("subtitle");
-const darkModeBtn = document.getElementById("dark_mode_btn");
-let darkMode = localStorage.getItem("darkMode");
+// const darkModeBtn = document.getElementById("dark_mode_btn");
+// let darkMode = localStorage.getItem("darkMode");
 
 const changeTitle = () => {
     title.textContent = "Don't you have anything better to do?";
@@ -13,13 +13,13 @@ title.addEventListener("click", changeTitle);
 subtitle.addEventListener("click", changeTitle);
 
 // Dark Mode
-darkModeBtn.addEventListener("load", function () {
-    if (localStorage.getItem("darkMode") === "true") {
-        darkModeBtn.textContent = "Switch Dark Mode";
-        darkMode = true;
-        return;
-    }
+// darkModeBtn.addEventListener("load", function () {
+//     if (localStorage.getItem("darkMode") === "true") {
+//         darkModeBtn.textContent = "Switch Dark Mode";
+//         darkMode = true;
+//         return;
+//     }
 
-    darkModeBtn.textContent = "Switch Light Mode";
-    darkMode = false;
-});
+//     darkModeBtn.textContent = "Switch Light Mode";
+//     darkMode = false;
+// });
